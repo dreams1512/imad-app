c8f190c19665374b4042efceb14d9a2601a4a666
dreams1512 <30730594+dreams1512@users.noreply.github.com>
2017-08-10 22:20:33 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.get('/article1', function (req, res) {
 });
 
 app.get('/article2', function (req, res) {
-  res.send("Article 2 requested");
+  res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 
 app.get('/article3', function (req, res) {
-  res.send("Article 3 requested");
+ res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
