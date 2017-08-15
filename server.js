0d2d07e258a65631527eefb9cd33a5e9f3fd6c07
dreams1512 <30730594+dreams1512@users.noreply.github.com>
2017-08-14 23:19:05 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/article3', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
-app.get('/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
