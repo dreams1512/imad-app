67ec11b0781fa9ca89399afc34db11d6280990bd
dreams1512 <30730594+dreams1512@users.noreply.github.com>
2017-08-14 23:41:13 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 alert ('modified main.js');
-var element = document.getElementById('main_text');
+var element = document.getElementById('main-text');
 element.innerHTML = "new text modified by main.js";
