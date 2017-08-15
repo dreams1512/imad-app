02cb26add5ac8e885fcc2d6c4186ed7fc72c4356
dreams1512 <30730594+dreams1512@users.noreply.github.com>
2017-08-14 23:51:50 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ alert ('modified main.js');
 var element = document.getElementById('main-text');
 element.innerHTML = "new text modified by main.js";
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft= marginLeft + 'px';
+}
 img.onclick=function() {
-  img.style.marginLeft='100px';
+  var interval = setInterval(moveRight,50);
 };
