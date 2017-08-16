9385441db4aa6c27d5c0180a7e9970c7dc889086
dreams1512 <30730594+dreams1512@users.noreply.github.com>
2017-08-15 23:59:29 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,21 @@ button.onclick = function(){
     request.open('GET', "http://dreams1512.imad.hasura-app.io/counter", true);
     request.send('null');
 };
+
+//submit name
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit= document.getElementById("submit_btn");
+submit.onclick = function(){
+  //make a request to the server and send the name
+  
+  //capture the list of name and render it as a undfined list
+  var names = ["name1","name2","name3","name4"];
+  var list = " ";
+  for (var i=1; i < names.length; i++)
+  {
+     list+= '<li>' + names[i] + '</li>'; 
+  }
+  
+  
+};
