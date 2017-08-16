5bad2f35d1926a8ded9c320da54f23fc7376b175
dreams1512 <30730594+dreams1512@users.noreply.github.com>
2017-08-15 23:32:10 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,19 @@
-console.log('Loaded!');
-//alert ('modified main.js');
-//var element = document.getElementById('main-text');
-//element.innerHTML = "new text modified by main.js";
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft= marginLeft + 'px';
-}
-img.onclick=function() {
-  var interval = setInterval(moveRight,50);
+//counter code 
+var button = document.getElementById('counter');
+button.onclick = function(){
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store in a variable
+    request.onreadystatechange = function() {
+       if(request.readyState === XMLHttpRequest.DONE && request.status === 200)
+       {
+         var counter = request.responseText;
+         var span = document.getElementById('count');
+         span.innerHTML = counter.toString();
+       } 
+    };
+    //make a request
+    request.open('GET', "http://dreams1512.imad.hasura-app.io/counter", true);
+    request.send('null');
 };
