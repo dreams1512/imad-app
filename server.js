9366ec42995a9cf2e4c5a30ba08470a9e876c8de
dreams1512 <30730594+dreams1512@users.noreply.github.com>
2017-08-29 11:30:30 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'dreams1512',
@@ -73,6 +74,20 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash(input, salt) {
+    //how do we create Hash -- we will use the default library Crypto - google -node crypto
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex'); // to return as string and hexadecimal encoding to readable on screen
+}
+
+app.get('/hash/:input', function(req,res){
+    
+    var hashedString = hash(req.params.input, 'this-is-some-random-salt');
+    res.hashesString();
+});
+
 
 var pool = new Pool(config);
 app.get('/test-db',function(req, res) {
