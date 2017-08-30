74706e576f54b68e8aa87c1d457d0f517443d9e5
dreams1512 <30730594+dreams1512@users.noreply.github.com>
2017-08-30 10:45:58 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/create-user', function(req,res) {
     var password = req.body.password;
     var salt = crypto.randomBytes(128).toString('hex');
     var dbString = hash(password, salt);
-    pool.query('INSERT INTO "user1" (username, password) VALUES ($1,$2)'), [username, dbString], function(err, result) {
+    pool.query('INSERT INTO "user1" (username, password) VALUES ($1,$2)', [username, dbString], function(err, result) {
         if (err)
             {
             res.status(500).send(err.toString());
@@ -107,7 +107,7 @@ app.post('/create-user', function(req,res) {
             {
             res.send("user added successfully "+ username);
             }
-    };
+    });
 });
     
 
